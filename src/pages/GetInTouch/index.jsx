2b9885400b65bbe31e043a9be29806264e2142ea
Wiliam Roza <devfrontwill@gmail.com>
2023-06-27 02:38:27 -0300

    FEAT: Criado e terminado a parte estatica da pagina de Contato e feita uma refatoração na pagina Home

--- src/pages/GetInTouch/index.jsx
+++ src/pages/GetInTouch/index.jsx
@@ -1,12 +1,43 @@
 import React from "react";
+import { PiCaretRightBold } from 'react-icons/pi';
 import styles from './styles.module.scss';
 
 
 function GetInTouch() {
     return (
-        <div>
-            <h1>Pagina de contato !</h1>
-        </div>
+        <>
+            <div className={styles.container} >
+                <h1 className={styles.title} >
+                    Get in Touch
+                </h1>
+                <span className={styles.subtitle} >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </span>
+
+                <form className={styles.form_container} >
+                    <label htmlFor="Email">Email :</label>
+                    <input className={styles.input_field}
+                        type="email" required placeholder="example@example.com" />
+
+                    <label htmlFor="phone">Telefone :</label>
+                    <input className={styles.input_field}
+                        type="tel" required placeholder="Ex. 21 98765-4321" minLength={11} maxLength={11} />
+
+                    <label htmlFor="message">Mensagem :</label>
+                    <input className={styles.input_msg} type="text" required placeholder="Digite aqui sua mensagem" />
+
+                    <button className={styles.btn_submit} type="submit">
+                        Submit
+                        <PiCaretRightBold 
+                        size={17}
+                        color="#FFF"
+                        />
+                    </button>
+                </form>
+            </div>
+
+            <footer className={styles.copyright} >© Copyright Devfrontwill - 2023 All rights reserved</footer>
+        </>
     )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,65 +1,74 @@
 import React from "react";
+import { LiaFileDownloadSolid } from 'react-icons/lia';
 import styles from './styles.module.scss';
 import profile from '../../assets/profile.png';
+import currilum from '../../assets/curriculum.pdf';
 
 function Home() {
     return (
         <>
-            <header>
-                <nav>
-                    <ul>
-                        <li>Home</li>
-                        <li>Case Studies</li>
-                        <li>Testimonials</li>
-                        <li>Recent work</li>
-                        <li>Get in Touch</li>
-                    </ul>
-                </nav>
-            </header>
-
-            <main>
-                <section>
-                    <div classs={styles.container_text}>
-                        <h1>
-                            Wiliam Roza
-                        </h1>
-                        <span>
-                            Sobre mim: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </span>
-                    </div>
-
-                    <div className={styles.container_img}>
-                        <img src={profile} alt="Foto de Wiliam Roza" />
-                    </div>
-
-                    <div className={styles.btn_container}>
-                        <button>
-                            Baixar CV
-                        </button>
-                    </div>
-                </section>
-            </main>
-
-            <footer>
-                <section className={styles.container_h3}>
-                    <h3>Worked with</h3>
-                </section>
-
-                <section>
+            <div className={styles.container}>
+                <header>
                     <nav>
-                        <ul className={styles.ul_footer} >
-                            <li className={styles.li_footer} >JavaScript</li>
-                            <li className={styles.li_footer} >React</li>
-                            <li className={styles.li_footer} >React Native</li>
-                            <li className={styles.li_footer} >Sass</li>
-                            <li className={styles.li_footer} >Git & Github</li>
-                            <li className={styles.li_footer} >TypeScript</li>
-                            <li className={styles.li_footer} >Scrum</li>
-                            <li className={styles.li_footer} >TailwindCSS</li>                       
+                        <ul className={styles.container_list}>
+                            <li className={styles.list_item} >Home</li>
+                            <li className={styles.list_item} >Case Studies</li>
+                            <li className={styles.list_item} >Testimonials</li>
+                            <li className={styles.list_item} >Recent work</li>
+                            <li className={styles.list_item} >Get in Touch</li>
                         </ul>
                     </nav>
-                </section>
-            </footer>
+                </header>
+
+                <main>
+                    <section className={styles.container_text}>
+                        <div>
+                            <h1 className={styles.title} >
+                                Wiliam Roza
+                            </h1>
+                            <span className={styles.subtitle} >
+                                Sobre mim: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </span>
+                        </div>
+
+                        <div className={styles.container_img}>
+                            <img src={profile} alt="Foto de Wiliam Roza" />
+                        </div>
+
+                        <div className={styles.btn_container}>
+                            <a href={currilum} download='currilum_devfrontwill={ Wiliam Roza }'>
+                                Baixar CV 
+                                <LiaFileDownloadSolid
+                                className={styles.icon}
+                                size={24}
+                                color="#FFF"
+                                />
+                            </a>
+                        </div>
+                    </section>
+                </main>
+
+                <footer>
+                    <section className={styles.container_h3}>
+                        <h3>Worked with:</h3>
+                    </section>
+
+                    <section>
+                        <nav>
+                            <ul className={styles.container_footer} >
+                                <li className={styles.footer_item} >JavaScript</li>
+                                <li className={styles.footer_item} >React</li>
+                                <li className={styles.footer_item} >React Native</li>
+                                <li className={styles.footer_item} >Sass</li>
+                                <li className={styles.footer_item} >Git & Github</li>
+                                <li className={styles.footer_item} >TypeScript</li>
+                                <li className={styles.footer_item} >Scrum</li>
+                                <li className={styles.footer_item} >TailwindCSS</li>
+                            </ul>
+                        </nav>
+                    </section>
+                </footer>
+            </div>
 
         </>
     )
