2e433ccf93857fcb2056453393ba3f594600a0e1
Wiliam Roza <devfrontwill@gmail.com>
2023-08-28 02:39:02 -0300

    feat: adicionado novos projetos ao portfolio e novas funcionalidades para melhor experiencia do usuário ao acessar meu portfolio

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -7,7 +7,7 @@ function Button({ title, href }) {
 
     return (
         <div className={styles.container__btn} >
-            <a className={styles.btn} href={href}>{title}
+            <a className={styles.btn} target='_blank' href={href}>{title}
                 <PiCaretRightBold
                     size={18}
                 />
--- src/components/ProjetosWeb/index.jsx
+++ src/components/ProjetosWeb/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from './styles.module.scss';
-import work1 from '../../assets/work1.png'
+import work from '../../assets/work1.png'
+import work1 from '../../assets/webdevlink.png';
+import work2 from '../../assets/devspace.png';
+import work3 from '../../assets/calcimc.png';
 import Button from "../Button";
 
 
@@ -15,14 +18,14 @@ function ProjetosWeb() {
             </span>
 
             <div className={styles.container__work} >
-                <h1 className={styles.title__work} >Work Name Here</h1>
+                <h1 className={styles.title__work} > <u>DevLink Shortener</u> </h1>
 
                 <span className={styles.subtitle__work}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut labore et dolore magna.
+                    DevLink Shortener é uma aplicação desenvolvida para que o usuário possa obter links encurtados com o objetivo de facilitar o uso de links extensos, seja para mostrar em um perfil de rede social ou afins. É uma aplicação dinâmica, que possui uma página inicial onde o usuário pode digitar ou colar o seu link que deseja encurtar, feito isso todo link encurtado é automaticamente armazenado na seção "meus links" onde o usuário consegue consultar a lista de links encurtados que ali ficam salvos, também é possivel deletar os links encurtados salvos que não forem mas utilizados. Foi utilizado a API do bit.ly, porém por ser FREE tem um baixo limite quantitativo para encurtamento de links. Dito isto, deixarei alguns prints da aplicação em funcionamento.
                 </span>
 
                 <div className={styles.container__img}>
-                    <img src={work1} alt="foto ilustrativa do projeto" className="img__right" />
+                    <img src={work1} alt="foto ilustrativa do projeto" />
                 </div>
 
                 <Button title="Acessar Projeto" href={"https://github.com/devfrontwill/devlink-shortener"} />
@@ -31,36 +34,36 @@ function ProjetosWeb() {
             </div>
 
             <div className={styles.container__work} >
-                <h1 className={styles.title__work} >Work Name Here</h1>
+                <h1 className={styles.title__work} ><u>Dev Space</u></h1>
 
                 <span className={styles.subtitle__work}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut labore et dolore magna.
+                    Dev Space é uma aplicação desenvolvida para um teste técnico de uma vaga de pessoa desenvolvedora front-end jr . Se trata de uma galeria de imagens criada para a empresa Alura com o objetivo de exibir as imagens incríveis que ela produz do universo observável. É uma aplicação estática, que possui apenas uma página inicial mas que é altamente escalável para adição de novas funcionalidades!
                 </span>
 
                 <div className={styles.container__img}>
-                    <img src={work1} alt="foto ilustrativa do projeto" className="img__right" />
+                    <img src={work2} alt="foto ilustrativa do projeto" />
                 </div>
 
 
-                <Button title="Acessar Projeto" />
+                <Button title="Acessar Projeto" href={"https://github.com/devfrontwill/DevSpace"} />
 
 
 
             </div>
 
             <div className={styles.container__work} >
-                <h1 className={styles.title__work} >Work Name Here</h1>
+                <h1 className={styles.title__work} > <u> DevCalc IMC </u></h1>
 
                 <span className={styles.subtitle__work}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut labore et dolore magna.
+                    Uma aplicação simples mas dinâmica feita apenas para praticar e exercitar o uso do Hook UseState do Reactjs, neste projeto eu criei uma calculadora de IMC com layout simples, porém responsivo.
                 </span>
 
                 <div className={styles.container__img}>
-                    <img src={work1} alt="foto ilustrativa do projeto" className="img__right" />
+                    <img src={work3} alt="foto ilustrativa do projeto" />
                 </div>
 
 
-                <Button title="Acessar Projeto" />
+                <Button title="Acessar Projeto" href={"https://github.com/devfrontwill/calc-imc_reactjs"} />
 
 
 
