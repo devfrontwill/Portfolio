c26ac447c218dfae4b10bdb6f838a9864e4f16ea
Wiliam Roza <devfrontwill@gmail.com>
2023-08-28 13:57:08 -0300

    Feat: criado o componente Card para melhor manutenção e adição de novos projetos

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+
+export default function Card({ title, subtitle, image }) {
+    return (
+
+        <div className={styles.container__work} >
+            <h1 className={styles.title__work} > <u>{title}</u> </h1>
+
+            <span className={styles.subtitle__work}> {subtitle} </span>
+
+            <div className={styles.container__img}>
+                <img src={image} alt="foto ilustrativa do projeto" />
+            </div>
+
+            
+
+
+        </div>
+    )
+}
--- src/components/ProjetosWeb/index.jsx
+++ src/components/ProjetosWeb/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from './styles.module.scss';
-import work from '../../assets/work1.png'
 import work1 from '../../assets/webdevlink.png';
 import work2 from '../../assets/devspace.png';
 import work3 from '../../assets/calcimc.png';
 import Button from "../Button";
+import Card from "../Card";
 
 
 function ProjetosWeb() {
@@ -17,57 +17,36 @@ function ProjetosWeb() {
             <span className={styles.subtitle}>Solving user & business problems since last 15+ years.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </span>
 
-            <div className={styles.container__work} >
-                <h1 className={styles.title__work} > <u>DevLink Shortener</u> </h1>
+            <Card
+                title="DevLink Shortener"
 
-                <span className={styles.subtitle__work}>
-                    DevLink Shortener é uma aplicação desenvolvida para que o usuário possa obter links encurtados com o objetivo de facilitar o uso de links extensos, seja para mostrar em um perfil de rede social ou afins. É uma aplicação dinâmica, que possui uma página inicial onde o usuário pode digitar ou colar o seu link que deseja encurtar, feito isso todo link encurtado é automaticamente armazenado na seção "meus links" onde o usuário consegue consultar a lista de links encurtados que ali ficam salvos, também é possivel deletar os links encurtados salvos que não forem mas utilizados. Foi utilizado a API do bit.ly, porém por ser FREE tem um baixo limite quantitativo para encurtamento de links. Dito isto, deixarei alguns prints da aplicação em funcionamento.
-                </span>
+                subtitle="DevLink Shortener é uma aplicação desenvolvida para que o usuário possa obter links encurtados com o objetivo de facilitar o uso de links extensos, seja para mostrar em um perfil de rede social ou afins. É uma aplicação dinâmica, que possui uma página inicial onde o usuário pode digitar ou colar o seu link que deseja encurtar, feito isso todo link encurtado é automaticamente armazenado na seção Meus Links onde o usuário consegue consultar a lista de links encurtados que ali ficam salvos, também é possivel deletar os links encurtados salvos que não forem mas utilizados. Foi utilizado a API do bit.ly, porém por ser FREE tem um baixo limite quantitativo para encurtamento de links. Dito isto, deixarei alguns prints da aplicação em funcionamento."
 
-                <div className={styles.container__img}>
-                    <img src={work1} alt="foto ilustrativa do projeto" />
-                </div>
+                image={work1}
+            />
 
-                <Button title="Acessar Projeto" href={"https://github.com/devfrontwill/devlink-shortener"} />
+            <Button title="Acessar Projeto" href={"https://github.com/devfrontwill/devlink-shortener"} />
 
+            <Card
+                title="Dev Space"
 
-            </div>
+                subtitle="Dev Space é uma aplicação desenvolvida para um teste técnico de uma vaga de pessoa desenvolvedora front-end jr . Se trata de uma galeria de imagens criada para a empresa Alura com o objetivo de exibir as imagens incríveis que ela produz do universo observável. É uma aplicação estática, que possui apenas uma página inicial mas que é altamente escalável para adição de novas funcionalidades!"
 
-            <div className={styles.container__work} >
-                <h1 className={styles.title__work} ><u>Dev Space</u></h1>
+                image={work2}
+            />
 
-                <span className={styles.subtitle__work}>
-                    Dev Space é uma aplicação desenvolvida para um teste técnico de uma vaga de pessoa desenvolvedora front-end jr . Se trata de uma galeria de imagens criada para a empresa Alura com o objetivo de exibir as imagens incríveis que ela produz do universo observável. É uma aplicação estática, que possui apenas uma página inicial mas que é altamente escalável para adição de novas funcionalidades!
-                </span>
+            <Button title="Acessar Projeto" href={"https://github.com/devfrontwill/DevSpace"} />
 
-                <div className={styles.container__img}>
-                    <img src={work2} alt="foto ilustrativa do projeto" />
-                </div>
+            <Card
+                title="DevCalc IMC"
 
+                subtitle="Uma aplicação simples mas dinâmica feita apenas para praticar e exercitar o uso do Hook UseState do Reactjs, neste projeto eu criei uma calculadora de IMC com layout simples, porém responsivo."
 
-                <Button title="Acessar Projeto" href={"https://github.com/devfrontwill/DevSpace"} />
+                image={work3}
+            />
 
+            <Button title="Acessar Projeto" href={"https://github.com/devfrontwill/calc-imc_reactjs"} />
 
-
-            </div>
-
-            <div className={styles.container__work} >
-                <h1 className={styles.title__work} > <u> DevCalc IMC </u></h1>
-
-                <span className={styles.subtitle__work}>
-                    Uma aplicação simples mas dinâmica feita apenas para praticar e exercitar o uso do Hook UseState do Reactjs, neste projeto eu criei uma calculadora de IMC com layout simples, porém responsivo.
-                </span>
-
-                <div className={styles.container__img}>
-                    <img src={work3} alt="foto ilustrativa do projeto" />
-                </div>
-
-
-                <Button title="Acessar Projeto" href={"https://github.com/devfrontwill/calc-imc_reactjs"} />
-
-
-
-            </div>
         </section>
     )
 }
